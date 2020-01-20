5a67ef5c415c8629d82fadfcf3d3c3b5b0149fee
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-01-20 22:17:13 +0800

    Create MessageModel.m

---
+++ MessageModel.m
@@ -0,0 +1,13 @@
+//
+//  MessageModel.m
+//  killcoldness
+//
+//  Created by ios group on 2020/1/20.
+//  Copyright © 2020年 s. All rights reserved.
+//
+
+#import "MessageModel.h"
+
+@implementation MessageModel
+
+@end
