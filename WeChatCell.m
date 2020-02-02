cde184d19cd3e2e58d50dbef0521aa8867018767
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-02-02 16:12:46 +0800

    Update WeChatCell.m
    添加了计算文本高度的方法 处理长按事件的方法 隐藏menuController的方法

--- WeChatCell.m
+++ WeChatCell.m
@@ -10,16 +10,16 @@
 #import "MessageModel.h"
 
 
-#define MAX_IMAGE_WH 141.0
 
 @implementation WeChatCell
 
-+(instancetype)cellWithTableView:(UITableView *)tableView messageMode:(MessageModel *)model
-{
+//Cell的初始化
++(instancetype)cellWithTableView:(UITableView *)tableView messageModel:(MessageModel *)model{
+    
     static NSString *identifier=@"WeChatCell";
 
     WeChatCell *cell=[tableView dequeueReusableCellWithIdentifier:identifier];
-    //
+    //cell的重用
     if (cell==nil)
     {
         cell=[[WeChatCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier messageModel:model];
@@ -27,6 +27,35 @@
     return cell;
 }
 
+//获取文本高度
++(CGFloat)tableHeightWithModel:(MessageModel *)model
+{
+    
+    CGFloat masTop=10;
+    if (model.showMessageTime)
+    {
+        masTop=37;
+    }
+    if (model.messageType==MessageTypeText)
+    {
+        CGFloat maxWith=AppFrameWidth-leftWith-rightWith-14-12-4;
+        
+        UIFont *textFont=[UIFont fontWithName:@"PingFangSC-Regular" size:16];
+        
+        NSDictionary *attributes=@{NSFontAttributeName:textFont};
+        CGRect rect=[model.messageText boundingRectWithSize:CGSizeMake(maxWith, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];//计算
+        
+        return rect.size.height+26+masTop+20;
+    }
+    else if (model.messageType==MessageTypeImage)
+    {
+        CGSize cgsize=[self imageShowSize:model.imageSmall];
+        return 40+cgsize.height+20;
+    }
+    return 0;
+}
+
+//自定义cell
 -(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier messageModel:(MessageModel*)model
 {
     
@@ -41,14 +70,15 @@
     for(UIView *view in [self.contentView subviews])
     {
         [view removeFromSuperview];
-    }//遍历移除默认的cell
+    }//遍历移除默认的cell组件
     
     CGFloat masTop=10;
     
-
     if (model.showMessageTime)
     {
         masTop=37;
+        
+        //设置时间标签
         UILabel *timeLabel=[[UILabel alloc] init];
         
         timeLabel.font=[UIFont fontWithName:@"PingfangSC-Regular" size:10];
@@ -58,11 +88,16 @@
         timeLabel.textAlignment=NSTextAlignmentCenter;//居中
         
         timeLabel.layer.masksToBounds=YES;
-        timeLabel.layer.cornerRadius=4;
+        timeLabel.layer.cornerRadius=4;//圆角
         timeLabel.layer.borderColor=(__bridge CGColorRef _Nullable)([UIColor colorWithRed:0.6 green:0.5 blue:1 alpha:0.5]);
         timeLabel.layer.borderWidth=1;
         [self.contentView addSubview:timeLabel];
+    }
         
+    else {
+        ;
+    }
+    //文本框图片
         UIImageView *contextBack=[[UIImageView alloc] init];
         contextBack.userInteractionEnabled=YES;
         
@@ -70,9 +105,8 @@
         self.coversImageView.userInteractionEnabled=YES;
         
         
-        CGFloat AppFrameWidth=[UIScreen mainScreen].bounds.size.width;
-        CGFloat leftWith=(AppFrameWidth>750?55:52.5);
-        CGFloat ringtWith=(AppFrameWidth>750?89:73);
+       
+
         
         
         if (model.messageType==MessageTypeText)
@@ -80,14 +114,12 @@
             if (model.messageSenderType==MessageSenderTypeZN)
             {
                 //头像
-                CGFloat maxWith=AppFrameWidth-leftWith-ringtWith-14-12-4;
+                CGFloat maxWith=AppFrameWidth-leftWith-rightWith-14-12-4;
                 UIImageView *logoImage=[[UIImageView alloc] init];
-                logoImage.frame=CGRectMake(10, masTop, 40, 40);
+                logoImage.frame=CGRectMake(10, masTop, 60, 60);
                 UIImage *u1=[UIImage imageNamed:@"1.png"];
                 [logoImage setImage:u1];
                 [self.contentView addSubview:logoImage];
-                
-                
                 
                 
                 UIFont *textFont=[UIFont fontWithName:@"PingFangSC-Regular" size:16];
@@ -101,7 +133,7 @@
                 //参数3:文字属性 通常是字体大小(要用字典存）
                 //参数4:绘制上下文 做底层排版时用，填nil
                 contextBack.frame=CGRectMake(leftWith, masTop, rect.size.width+26, rect.size.height+26);
-                contextBack.image=[[UIImage imageNamed:@"3"] stretchableImageWithLeftCapWidth:10 topCapHeight:25];
+                contextBack.image=[[UIImage imageNamed:@"4"] stretchableImageWithLeftCapWidth:10 topCapHeight:25];
                 [self.contentView addSubview:contextBack];
                 
                 
@@ -118,12 +150,12 @@
             
             else if (model.messageSenderType==MessageSenderTypeUser)
             {
-                //
-                CGFloat maxWith=AppFrameWidth-leftWith-ringtWith-14-12-4;
+                //头像
+                CGFloat maxWith=AppFrameWidth-leftWith-rightWith-14-12-4;
                 
                 UIImageView *logoImage=[[UIImageView alloc] init];
-                logoImage.frame=CGRectMake(AppFrameWidth-10-40, masTop, 40, 40);
-                [logoImage setImage:[UIImage imageNamed:@"4"]];
+                logoImage.frame=CGRectMake(AppFrameWidth-10-60, masTop, 60, 60);
+                [logoImage setImage:[UIImage imageNamed:@"2"]];
                 [self.contentView addSubview:logoImage];
                 
                 
@@ -134,7 +166,7 @@
                 
                 
                 contextBack.frame=CGRectMake(AppFrameWidth-(rect.size.width+26)-leftWith+12, masTop, rect.size.width+26, rect.size.height+26);
-                contextBack.image=[[UIImage imageNamed:@"4"] stretchableImageWithLeftCapWidth:10 topCapHeight:25];
+                contextBack.image=[[UIImage imageNamed:@"3"] stretchableImageWithLeftCapWidth:10 topCapHeight:25];
                 [self.contentView addSubview: contextBack];
                 
                 
@@ -154,7 +186,7 @@
             if (model.messageSenderType==MessageSenderTypeZN)
             {
                 UIImageView *logoImage=[[UIImageView alloc] init];
-                logoImage.frame=CGRectMake(10, masTop, 40, 40);
+                logoImage.frame=CGRectMake(10, masTop, 60, 60);
                 [logoImage setImage:[UIImage imageNamed:@"1"]];
                 [self.contentView addSubview:logoImage];
                 
@@ -190,28 +222,44 @@
                 contextBack.layer.mask=imageViewMask.layer;
                 [self.contentView addSubview:imageViewMask];
                 
-                
-                
             }
         }
-        
-        
-        
+        //长按事件
         UILongPressGestureRecognizer *longPressGesture=[[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressBubbleView:)];
+        [contextBack addGestureRecognizer:longPressGesture];
         
+        UITapGestureRecognizer *singleTap=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap1:)];
+        UITapGestureRecognizer *doubleTap=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+        [doubleTap setNumberOfTapsRequired:2];
+        [singleTap requireGestureRecognizerToFail:doubleTap];
+        [contextBack addGestureRecognizer:doubleTap];
+        [contextBack addGestureRecognizer:singleTap];
         
+        self.coversImageView.frame=contextBack.frame;
         
+        if (model.messageSenderType==MessageSenderTypeZN)
+        {
+            self.coversImageView.image=[[UIImage imageNamed:@"4"]stretchableImageWithLeftCapWidth:10 topCapHeight:25];
+        }
         
-    }
-    
-    
-    
+        else
+        {
+            self.coversImageView.image=[[UIImage imageNamed:@"3"]stretchableImageWithLeftCapWidth:10 topCapHeight:25];
+        }
+        
+        [self.contentView addSubview:self.coversImageView];
+        self.coversImageView.hidden=YES;
+        
+        UITapGestureRecognizer *singleTap2=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap2:)];
+        [self.coversImageView addGestureRecognizer:singleTap2];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(menuControllerWillHidden:) name:UIMenuControllerWillHideMenuNotification object:nil];
     
     return self;
 }
                                                                                                                                                                                                                                                                                             
 
-
+//获取图片尺寸
 +(CGSize)imageShowSize:(UIImage *)image
 {
     CGFloat imageWide=image.size.width;
@@ -230,7 +278,7 @@
     return CGSizeZero;
 }
 
-
+//长按后的阴影
 -(void)longPressBubbleView:(UILongPressGestureRecognizer *)sender
 {
     if (sender.state==UIGestureRecognizerStateBegan)
@@ -239,6 +287,7 @@
     }
 }
 
+//设置menuController
 -(void)showMenuControllerInView:(WeChatCell *)inView bgView:(UIView *)supView
 {
     
@@ -248,31 +297,29 @@
     MessageModel *messageModel=self.messageModel;
     
     UIMenuItem *copyTextItem1=[[UIMenuItem alloc] initWithTitle:@"复制" action:@selector(copyTextSender1:)];
-    UIMenuItem *copyTextItem2=[[UIMenuItem alloc] initWithTitle:@"删除" action:@selector(copyTextSender2:)];
-    UIMenuItem *copyTextItem3=[[UIMenuItem alloc] initWithTitle:@"保存" action:@selector(copyTextSender3:)];
-    UIMenuItem *copyTextItem4=[[UIMenuItem alloc] initWithTitle:@"多选" action:@selector(copyTextSender4:)];
+    UIMenuItem *deleteTextItem2=[[UIMenuItem alloc] initWithTitle:@"删除" action:@selector(deleteTextSender2:)];
+    UIMenuItem *saveTextItem3=[[UIMenuItem alloc] initWithTitle:@"保存" action:@selector(saveTextSender3:)];
+    UIMenuItem *selectTextItem4=[[UIMenuItem alloc] initWithTitle:@"多选" action:@selector(selectTextSender4:)];
     
     UIMenuController *menu=[UIMenuController sharedMenuController];
+    
     [menu setTargetRect:supView.frame inView:inView];
     [menu setArrowDirection:UIMenuControllerArrowDefault];
     
     if (messageModel.messageType==MessageTypeText)
     {
-        [menu setMenuItems:@[copyTextItem1,copyTextItem2,copyTextItem4]];
+        [menu setMenuItems:@[copyTextItem1,deleteTextItem2,selectTextItem4]];
     }
     else if(messageModel.messageType==MessageTypeImage)
     {
-        [menu setMenuItems:@[copyTextItem1,copyTextItem2,copyTextItem3,copyTextItem4]];
-        
+        [menu setMenuItems:@[copyTextItem1,deleteTextItem2,saveTextItem3,selectTextItem4]];
     }
     
     [menu setMenuVisible:YES animated:YES];
-    
-    
 }
 
 
-
+//剪贴板
 -(BOOL)canBecomeFirstResponder
 {
     return YES;
@@ -282,7 +329,7 @@
 {
     if (action==@selector(copyTextSender1:))
         return true;
-    else if(action==@selector(copyTextSender2:))
+    else if(action==@selector(deleteTextSender2:))
         return true;
     else
         return false;
@@ -295,20 +342,51 @@
     
 }
 
--(void)copyTextSender2:(id)sender
+-(void)deleteTextSender2:(id)sender
 {
     
 }
 
--(void)copyTextSender3:(id)sender
+-(void)saveTextSender3:(id)sender
 {
     
 }
--(void)copyTextSender4:(id)sender
+-(void)selectTextSender4:(id)sender
 {
     
 }
 
+-(void)copyImaggeSender:(id)sender
+{
+    //UIImageWriteToSavedPhotosAlbum(<#UIImage * _Nonnull image#>, <#id  _Nullable completionTarget#>, <#SEL  _Nullable completionSelector#>, <#void * _Nullable contextInfo#>)
+}
+
+-(void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+}
+
+
+-(void)handleSingleTap1:(id)sender
+{
+    
+}
+
+-(void)handleSingleTap2:(id)sender
+{
+    
+}
+
+-(void)handleDoubleTap:(id)sender
+{
+    
+}
+
+-(void)menuControllerWillHidden:(id)sender
+{
+    if (!self.coversImageView.isHidden) {
+        self.coversImageView.hidden=YES;
+    }
+}
 
 - (void)awakeFromNib {
     [super awakeFromNib];
