942653cf33ec6d76deaa490dc3c8af2f16d0252f
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-02-04 23:03:37 +0800

    Create NextViewController.h

---
+++ NextViewController.h
@@ -0,0 +1,17 @@
+//
+//  NextViewController.h
+//  killcoldness
+//
+//  Created by ios group on 2020/2/3.
+//  Copyright © 2020年 s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NextViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
