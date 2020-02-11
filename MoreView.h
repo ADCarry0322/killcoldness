5c2e8d9b858f5436035e0ad9c74d93eb7caecf2f
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-02-11 22:05:09 +0800

    Create MoreView.h
    更多 按钮

---
+++ MoreView.h
@@ -0,0 +1,17 @@
+//
+//  MoreView.h
+//  killcoldness
+//
+//  Created by ios group on 2020/2/5.
+//  Copyright © 2020年 s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoreView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
