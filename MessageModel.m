714f99b7ba256169f2968277f32c8fca0ffb705e
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-02-12 20:28:04 +0800

    Update MessageModel.m

--- MessageModel.m
+++ MessageModel.m
@@ -10,4 +10,20 @@
 
 @implementation MessageModel
 
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+    [aCoder encodeObject:_messageTime forKey:@"time"];
+    [aCoder encodeObject:_messageText forKey:@"text"];
+    [aCoder encodeObject:_imageSmall forKey:@"image"];
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)aDecoder {
+    self=[super init];
+    if (self) {
+        _messageTime=[aDecoder decodeObjectForKey:@"time"];
+        _messageText=[aDecoder decodeObjectForKey:@"text"];
+        _imageSmall=[aDecoder decodeObjectForKey:@"image"];
+    }
+    return self;
+}
+
 @end
