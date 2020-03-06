278ad78d8240177e871a4c2a2a2ed69ecb7e58ba
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-03-06 21:16:28 +0800

    Update MessageModel.m
    遵循NSSecureCoding协议

--- MessageModel.m
+++ MessageModel.m
@@ -7,23 +7,35 @@
 //
 
 #import "MessageModel.h"
-
+#import "ViewController.h"
 @implementation MessageModel
 
 - (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+    [aCoder encodeBool:_showMessageTime forKey:@"showTime"];
+    [aCoder encodeInteger:(NSInteger)_messageSenderType forKey:@"messageSender"];
+    [aCoder encodeInteger:(NSInteger)_messageType forKey:@"messageType"];
     [aCoder encodeObject:_messageTime forKey:@"time"];
     [aCoder encodeObject:_messageText forKey:@"text"];
     [aCoder encodeObject:_imageSmall forKey:@"image"];
+
 }
 
 - (nullable instancetype)initWithCoder:(nonnull NSCoder *)aDecoder {
     self=[super init];
     if (self) {
+        _showMessageTime=[aDecoder decodeBoolForKey:@"showTime"];
+        _messageSenderType=[aDecoder decodeIntegerForKey:@"messageSender"];
+        _messageType=[aDecoder decodeIntegerForKey:@"messageType"];
         _messageTime=[aDecoder decodeObjectForKey:@"time"];
         _messageText=[aDecoder decodeObjectForKey:@"text"];
         _imageSmall=[aDecoder decodeObjectForKey:@"image"];
     }
     return self;
 }
+
++(BOOL)supportsSecureCoding{
+    return YES;
+}
+
 
 @end
