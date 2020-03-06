07536762d53328387d825cbb3ecfb5a5bf55cbf2
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-03-06 21:19:57 +0800

    Update ViewController.h
    添加keyboardView

--- ViewController.h
+++ ViewController.h
@@ -8,12 +8,20 @@
 
 #import <UIKit/UIKit.h>
 #import "MessageModel.h"
+#import "Keyboard/KeyboardView.h"
+#import "Keyboard/MoreView.h"
+#import "Keyboard/EmojiView.h"
+#import "Keyboard/TextView.h"
+#import "Keyboard/UIView+FrameTool.h"
 
 @interface ViewController : UIViewController
 
-@property(nonatomic,strong)UITableView *table;
+
+
+@property(nonatomic,retain)UITableView *table;
 
 @property(nonatomic,strong) NSMutableArray<MessageModel *>*arrList;
 
+@property (nonatomic,strong)KeyboardView *keyView;
 @end
 
